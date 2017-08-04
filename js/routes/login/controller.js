f659891464ade082410b02a44361ad3b41f59499
Gerard Carbonell <qiete.qiete@gmail.com>
2017-08-04 16:20:52 +0200

    image

--- js/routes/login/controller.js
+++ js/routes/login/controller.js
@@ -1,17 +1,17 @@
 angular.module('skyStream')
-    .controller('loginController', function($scope, $rootScope, $location, userService) {
-        $rootScope.auth = true
+    .controller('loginController', function ($scope, $rootScope, $location, userService) {
+      $rootScope.auth = true
 
-        $scope.login = function() {
-            var username = $scope.username;
-            var password = $scope.password;
+      $scope.login = function () {
+        var username = $scope.username
+        var password = $scope.password
 
-            userService.login(username, password, function(result) {
-            	if (result.status === 'LOGIN_SUCCEEDED') {
-            		$location.path('/home')
+        userService.login(username, password, function (result) {
+                if (result.status === 'LOGIN_SUCCEEDED') {
+                    $location.path('/home')
 
-            		$scope.$apply()
-            	}
-            })
-        }
+                    $scope.$apply()
+                }
+        })
+      }
     })

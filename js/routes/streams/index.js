e9bb70c996bac7991c6b47e22754527febf15c0b
Manuel Barzi <manuelbarzi@Manuels-MacBook-Pro.local>
2017-08-04 09:40:42 +0200

    restrict top-videos access to logged-in users only

--- js/routes/streams/index.js
+++ js/routes/streams/index.js
@@ -1,8 +1,18 @@
 angular.module('skyStream')
-.config(function ($routeProvider) {
-  $routeProvider
-  .when('/streams', {
-    templateUrl: 'js/routes/streams/template.html',
-    controller: 'getStreamsController'
-  })
-})
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/streams', {
+                templateUrl: 'js/routes/streams/template.html',
+    			controller: 'getStreamsController',
+                resolve: {
+                    check: function($location, userService) {
+                        var user = userService.retrieve();
+                        if (user) {
+                            $location.path('/streams')
+                        } else {
+                            $location.path('/')
+                        }
+                    }
+                }
+            })
+    })
--- js/routes/top-videos/index.js
+++ js/routes/top-videos/index.js
@@ -1,8 +1,18 @@
 angular.module('skyStream')
-.config(function ($routeProvider) {
-  $routeProvider
-  .when('/top-videos', {
-    templateUrl: 'js/routes/top-videos/template.html',
-    controller: 'getTopVideosController'
-  })
-})
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/top-videos', {
+                templateUrl: 'js/routes/top-videos/template.html',
+                controller: 'getTopVideosController',
+                resolve: {
+                    check: function($location, userService) {
+                        var user = userService.retrieve();
+                        if (user) {
+                            $location.path('/top-videos')
+                        } else {
+                            $location.path('/')
+                        }
+                    }
+                }
+            })
+    })

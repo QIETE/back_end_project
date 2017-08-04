5459de836431e7a6cdc157c5ecb22668e5c4224c
Gerard Carbonell <qiete.qiete@gmail.com>
2017-08-04 13:14:06 +0200

    created my account view

--- js/routes/myAccount/controller.js
+++ js/routes/myAccount/controller.js
@@ -1,6 +1,12 @@
 angular.module('skyStream')
 
-  .controller('myAccountController', function ($scope, $rootScope, $location) {
-    $rootScope.auth = true
-  }
-  )
+    .controller('myAccountController', function ($scope, $rootScope, $location, userService) {
+      $rootScope.auth = true
+      $scope.name = userService.retrieve().name
+      $scope.surname = userService.retrieve().surname
+      $scope.username = userService.retrieve().username
+      $scope.password = userService.retrieve().password
+      $scope.email = userService.retrieve().email
+      $scope.age = userService.retrieve().age
+      $scope.country = userService.retrieve().country
+    })

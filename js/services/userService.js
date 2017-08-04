bea54ec0e62c44ae7aaf9325ca8a2edf3f6d592e
Gerard Carbonell <qiete.qiete@gmail.com>
2017-08-04 17:02:39 +0200

    update xcerated

--- js/services/userService.js
+++ js/services/userService.js
@@ -1,33 +1,33 @@
 angular.module('skyStream')
-    .factory('userService', function(userFirebaseService) {
+    .factory('userService', function (userFirebaseService) {
+      var user
 
-        var user;
+      function login (username, password, callback) {
+        userFirebaseService.login(username, password, function (login) {
+          if (login.status === 'LOGIN_SUCCEEDED') {
+            userFirebaseService.retrieve(username, function (retrieve) {
+              if (retrieve.status === 'RETRIEVE_SUCCEEDED') {
+                user = retrieve.data
 
-        function login(username, password, callback) {
-            userFirebaseService.login(username, password, function(login) {
-                if (login.status === 'LOGIN_SUCCEEDED') {
-                    userFirebaseService.retrieve(username, function(retrieve) {
-                        if (retrieve.status === 'RETRIEVE_SUCCEEDED') {
-                            user = retrieve.data
-                            
-                            callback(login)
-                        }
-                    })
-                } else {
-                    callback(login)
-                }
+                callback(login)
+              }
             })
-        }
+          } else {
+            callback(login)
+          }
+        })
+      }
 
-        function retrieve() {
-          return user;
-        }
+      function retrieve () {
+        return user
+      }
 
-        var userService = {
-            register: userFirebaseService.register,
-            login: login,
-            retrieve: retrieve
-        }
+      var userService = {
+        register: userFirebaseService.register,
+        login: login,
+        retrieve: retrieve,
+        update: userFirebaseService.update
+      }
 
-        return userService;
+      return userService
     })

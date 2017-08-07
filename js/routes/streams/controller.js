6ea3fad7a9cc640ed1a376529dcc068b0a1b544e
Gerard Carbonell <qiete.qiete@gmail.com>
2017-08-07 16:03:57 +0200

    no me sale

--- js/routes/streams/controller.js
+++ js/routes/streams/controller.js
@@ -1,26 +1,27 @@
 angular.module('skyStream')
-.controller('getStreamsController', function ($scope, $rootScope, DataService, $sce) {
-  $rootScope.auth = true
-  $scope.section = 'TOP STREAMS'
-  $scope.notGame = ''
-  DataService.getStreams()
-  .then(function (oData) {
-    oData.data.streams.forEach(function (key) {
-      let url = 'https://player.twitch.tv/?channel=' + key.channel.display_name + '&autoplay=false'
-      key.channel.display_name = $sce.trustAsResourceUrl(url)
-    })
-    $scope.names = oData.data.streams
-    if ($scope.names.length === 0) {
-      $scope.notGame += $routeParams.query + 'not found'
-    } else {
-      $scope.names = oData.data.streams
-    }
-  })
-  $scope.like = function () {
-    return userService.like()
-  }
+    .controller('getStreamsController', function ($scope, $rootScope, DataService, $sce, userService) {
+      $rootScope.auth = true
+      $scope.section = 'TOP STREAMS'
+      $scope.notGame = ''
+      DataService.getStreams()
+            .then(function (oData) {
+              oData.data.streams.forEach(function (key) {
+                let url = 'https://player.twitch.tv/?channel=' + key.channel.display_name + '&autoplay=false'
+                key.channel.display_name = $sce.trustAsResourceUrl(url)
+              })
+              $scope.names = oData.data.streams
+              if ($scope.names.length === 0) {
+                $scope.notGame += $routeParams.query + 'not found'
+              } else {
+                $scope.names = oData.data.streams
+              }
+            })
+      $scope.like = function (contentId) {
+        console.log($scope.names._id)
+        return userService.like('streams', contentId)
+      }
 
-  $scope.dislike = function () {
-    return userService.dislike()
-  }
-})
+      $scope.dislike = function (contentId) {
+        return userService.dislike('streams', contentId)
+      }
+    })

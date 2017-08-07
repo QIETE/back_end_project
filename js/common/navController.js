e0b0317d88c63a51b0f4183ba60233f3adb3baad
Gerard Carbonell <qiete.qiete@gmail.com>
2017-08-07 10:24:28 +0200

    hello

--- js/common/navController.js
+++ js/common/navController.js
@@ -1,6 +1,11 @@
 angular.module('skyStream')
-    .controller('navController', function($scope, userService) {
-        $scope.isUserLoggedIn = function() {
-        	return !!userService.retrieve();
-        }
+    .controller('navController', function ($scope, userService) {
+      $scope.isUserLoggedIn = function () {
+        return !!userService.retrieve()
+      }
+
+      $scope.logout = function () {
+      	return userService.logout()
+      	console.log('hola mundo')
+      }
     })

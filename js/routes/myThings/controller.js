cb6dd9d94ed4a1c7ef16368597fc67b22f7fd3e1
Gerard Carbonell <qiete.qiete@gmail.com>
2017-08-08 12:46:59 +0200

    h

--- js/routes/myThings/controller.js
+++ js/routes/myThings/controller.js
@@ -1,4 +1,13 @@
 angular.module('skyStream')
-    .controller('myThingsController', function ($scope, $rootScope) {
+    .controller('myThingsController', function ($scope, $rootScope, userService) {
       $rootScope.auth = true
+      // $scope.likes
+      // $scope.dislikes
+      var user = userService.retrieve()
+      var analytics = user.analytics
+      var games = user.analytics.games
+      var streams = user.analytics.streams
+      var videos = user.analytics.videos
+
+      	  $scope.games = games
     })
--- js/services/userFirebaseService.js
+++ js/services/userFirebaseService.js
@@ -1,139 +1,139 @@
 angular.module('skyStream')
-    .factory('userFirebaseService', function() {
-        var config = {
-            apiKey: 'AIzaSyBiVj6w14u24CUoVAchlWv_NO9A6NeAwkk',
-            authDomain: 'skystream-66015.firebaseapp.com',
-            databaseURL: 'https://skystream-66015.firebaseio.com',
-            projectId: 'skystream-66015',
-            storageBucket: 'skystream-66015.appspot.com',
-            messagingSenderId: '908767214855'
-        }
+    .factory('userFirebaseService', function () {
+      var config = {
+        apiKey: 'AIzaSyBiVj6w14u24CUoVAchlWv_NO9A6NeAwkk',
+        authDomain: 'skystream-66015.firebaseapp.com',
+        databaseURL: 'https://skystream-66015.firebaseio.com',
+        projectId: 'skystream-66015',
+        storageBucket: 'skystream-66015.appspot.com',
+        messagingSenderId: '908767214855'
+      }
 
-        firebase.initializeApp(config)
-        var database = firebase.database()
+      firebase.initializeApp(config)
+      var database = firebase.database()
 
-        function register(user, callback) {
-            var ref = database.ref('users/' + user.username)
+      function register (user, callback) {
+        var ref = database.ref('users/' + user.username)
 
-            ref.once('value', function(result) {
-                if (result.val()) {
-                    callback({
-                        status: 'ALREADY_REGISTERED',
-                        message: 'user already registered'
-                    })
-                } else {
-                    var data = {
-                        name: user.name,
-                        surname: user.surname,
-                        age: user.age,
-                        password: user.password,
-                        country: user.country,
-                        email: user.email
-                    }
-
-                    var ref = database.ref('users')
-                    database.ref('users').child(user.username).set(data)
-                    callback({
-                        status: 'REGISTER_SUCCEEDED',
-                        message: 'user successfully registered'
-                    })
-                }
+        ref.once('value', function (result) {
+          if (result.val()) {
+            callback({
+              status: 'ALREADY_REGISTERED',
+              message: 'user already registered'
             })
-        }
+          } else {
+            var data = {
+              name: user.name,
+              surname: user.surname,
+              age: user.age,
+              password: user.password,
+              country: user.country,
+              email: user.email
+            }
 
-        function login(username, password, callback) {
-            var ref = database.ref('users/' + username)
-
-            ref.once('value', function(result) {
-                if (!result.val()) {
-                    callback('user doesn´t exists')
-                } else {
-                    if (result.val().password === password) {
-                        callback({
-                            status: 'LOGIN_SUCCEEDED',
-                            message: 'user successfully logged in'
-                        })
-                    } else {
-                        callback({
-                            status: 'LOGIN_FAILED',
-                            message: 'username and / or password incorrect'
-                        })
-                    }
-                }
+            var ref = database.ref('users')
+            database.ref('users').child(user.username).set(data)
+            callback({
+              status: 'REGISTER_SUCCEEDED',
+              message: 'user successfully registered'
             })
-        }
+          }
+        })
+      }
 
-        function retrieve(username, callback) {
-            var ref = database.ref('users/' + username)
-            ref.once('value', function(result) {
-                if (result.val()) {
-                    var user = {
-                        name: result.val().name,
-                        surname: result.val().surname,
-                        age: result.val().age,
-                        username: username,
-                        password: result.val().password,
-                        country: result.val().country,
-                        email: result.val().email
+      function login (username, password, callback) {
+        var ref = database.ref('users/' + username)
 
-                    }
-                    callback({
-                        status: 'RETRIEVE_SUCCEEDED',
-                        message: 'user successfully retrieved',
-                        data: user
-                    })
-                } else {
-                    callback({
-                        status: 'RETRIEVE_FAILED',
-                        message: 'user could not be retrieved'
-                    })
-                }
+        ref.once('value', function (result) {
+          if (!result.val()) {
+            callback('user doesn´t exists')
+          } else {
+            if (result.val().password === password) {
+              callback({
+                status: 'LOGIN_SUCCEEDED',
+                message: 'user successfully logged in'
+              })
+            } else {
+              callback({
+                status: 'LOGIN_FAILED',
+                message: 'username and / or password incorrect'
+              })
+            }
+          }
+        })
+      }
+
+      function retrieve (username, callback) {
+        var ref = database.ref('users/' + username)
+        ref.once('value', function (result) {
+          if (result.val()) {
+            var user = {
+              name: result.val().name,
+              surname: result.val().surname,
+              age: result.val().age,
+              username: username,
+              password: result.val().password,
+              country: result.val().country,
+              email: result.val().email,
+              analytics: result.val().analytics
+
+            }
+            callback({
+              status: 'RETRIEVE_SUCCEEDED',
+              message: 'user successfully retrieved',
+              data: user
             })
-        }
-
-        function update(user, callback) {
-            var ref = database.ref('users/' + user.username)
-
-            ref.once('value', function(result) {
-                var data = {
-                    name: user.name,
-                    surname: user.surname,
-                    age: user.age,
-                    password: user.password,
-                    country: user.country,
-                    email: user.email
-                }
-
-                var ref = database.ref('users')
-                database.ref('users').child(user.username).set(data)
-                callback({
-                    status: 'UPDATE_SUCCEEDED',
-                    message: 'user successfully updated'
-                })
+          } else {
+            callback({
+              status: 'RETRIEVE_FAILED',
+              message: 'user could not be retrieved'
             })
-        }
+          }
+        })
+      }
 
-        function like(username, contentType, contentId, contentName) {
-            var path = '/analytics/' + contentType + '/' + contentId;
-            database.ref('users/' + username).child(path + '/choice').set(1)
-            database.ref('users/' + username).child(path + '/title').set(contentName)
-        }
+      function update (user, callback) {
+        var ref = database.ref('users/' + user.username)
 
-        function dislike(username, contentType, contentId, contentName) {
-            var path = '/analytics/' + contentType + '/' + contentId;
-            database.ref('users/' + username).child(path + '/choice').set(-1)
-            database.ref('users/' + username).child(path + '/title').set(contentName)
-        }
+        ref.once('value', function (result) {
+          var data = {
+            name: user.name,
+            surname: user.surname,
+            age: user.age,
+            password: user.password,
+            country: user.country,
+            email: user.email
+          }
 
-        var userFirebaseService = {
-            register: register,
-            login: login,
-            retrieve: retrieve,
-            update: update,
-            like: like,
-            dislike: dislike
-        }
+          var ref = database.ref('users')
+          database.ref('users').child(user.username).set(data)
+          callback({
+            status: 'UPDATE_SUCCEEDED',
+            message: 'user successfully updated'
+          })
+        })
+      }
 
-        return userFirebaseService
+      function like (username, contentType, contentId, contentName) {
+        var path = '/analytics/' + contentType + '/' + contentId
+        database.ref('users/' + username).child(path + '/choice').set(1)
+        database.ref('users/' + username).child(path + '/title').set(contentName)
+      }
 
+      function dislike (username, contentType, contentId, contentName) {
+        var path = '/analytics/' + contentType + '/' + contentId
+        database.ref('users/' + username).child(path + '/choice').set(-1)
+        database.ref('users/' + username).child(path + '/title').set(contentName)
+      }
+
+      var userFirebaseService = {
+        register: register,
+        login: login,
+        retrieve: retrieve,
+        update: update,
+        like: like,
+        dislike: dislike
+      }
+
+      return userFirebaseService
     })

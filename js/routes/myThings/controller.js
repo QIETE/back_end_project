b711b9135cc07c5383410be628478019c945f39f
Manuel Barzi <manuelbarzi@Manuels-MacBook-Pro.local>
2017-08-08 15:17:03 +0200

    correct like / dislikes listing presentation logic

--- js/routes/myThings/controller.js
+++ js/routes/myThings/controller.js
@@ -11,9 +11,25 @@ angular.module('skyStream')
         var streams = user.analytics.streams
         var videos = user.analytics.videos
 
-        $scope.games = games
+        var likeGames
+        var dislikeGames
 
-        
+        for (var id in games) {
+            var game = games[id]
 
+            if (game.choice === 1) {
+                if (likeGames === undefined) {
+                    likeGames = {}
+                }
+                likeGames[id] = game;
+            } else {
+                if (dislikeGames === undefined) {
+                    dislikeGames = {}
+                }
+                dislikeGames[id] = game;
+            }
+        }
 
+        $scope.likeGames = likeGames
+        $scope.dislikeGames = dislikeGames
     })

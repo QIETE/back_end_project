f2af89f2bd5b5a0937254a4697ec3714d45617e4
Gerard Carbonell <qiete.qiete@gmail.com>
2017-08-10 12:53:37 +0200

    photos solved

--- js/common/navController.js
+++ js/common/navController.js
@@ -1,16 +1,20 @@
 angular.module('skyStream')
     .controller('navController', function ($scope, $location, userService) {
-        $scope.isUserLoggedIn = function () {
-            return userService.isLoggedIn()
+      $scope.isUserLoggedIn = function () {
+        return userService.isLoggedIn()
+      }
+
+      $scope.photo = function () {
+        return userService.photo()
+      }
+
+      $scope.logout = function () {
+        if (confirm('Are you sure you want to logout?')) {
+          userService.logout()
+
+          $location.path('/')
+
+                // $scope.$apply()
         }
-
-        $scope.logout = function () {
-            if (confirm('Are you sure you want to logout?')) {
-                userService.logout()
-
-                $location.path('/')
-
-                //$scope.$apply()
-            }
-        }
+      }
     })
--- js/services/userService.js
+++ js/services/userService.js
@@ -1,56 +1,62 @@
 angular.module('skyStream')
     .factory('userService', function (firebaseService) {
-        function login(username, password, callback) {
-            firebaseService.login(username, password, function (login) {
-                if (login.status === 'LOGIN_SUCCEEDED') {
-                    firebaseService.retrieve(username, function (retrieve) {
-                        if (retrieve.status === 'RETRIEVE_SUCCEEDED') {
-                            sessionStorage.skyStream_username = username
-                            sessionStorage.skyStream_loggedIn = true
+      function login (username, password, callback) {
+        firebaseService.login(username, password, function (login) {
+          if (login.status === 'LOGIN_SUCCEEDED') {
+            firebaseService.retrieve(username, function (retrieve) {
+              if (retrieve.status === 'RETRIEVE_SUCCEEDED') {
+                sessionStorage.skyStream_username = username
+                sessionStorage.skyStream_photo = retrieve.data.photo
+                sessionStorage.skyStream_loggedIn = true
 
-                            callback(login)
-                        }
-                    })
-                } else {
-                    callback(login)
-                }
+                callback(login)
+              }
             })
-        }
+          } else {
+            callback(login)
+          }
+        })
+      }
 
-        function isLoggedIn() {
-            return sessionStorage.skyStream_loggedIn
-        }
+      function photo () {
+        return sessionStorage.skyStream_photo
+      }
 
-        function logout() {
-            delete sessionStorage.skyStream_username
-            delete sessionStorage.skyStream_loggedIn
-        }
+      function isLoggedIn () {
+        return sessionStorage.skyStream_loggedIn
+      }
 
-        function retrieve(callback) {
-            firebaseService.retrieve(sessionStorage.skyStream_username, function(result) {
-                callback(result.data)
-            })
-        }
+      function logout () {
+        delete sessionStorage.skyStream_username
+        delete sessionStorage.skyStream_loggedIn
+      }
 
-        function like(contentType, contentId, contentName) {
-            firebaseService.like(sessionStorage.skyStream_username, contentType, contentId, contentName)
-        }
+      function retrieve (callback) {
+        firebaseService.retrieve(sessionStorage.skyStream_username, function (result) {
+          callback(result.data)
+        })
+      }
 
-        function dislike(contentType, contentId, contentName) {
-            firebaseService.dislike(sessionStorage.skyStream_username, contentType, contentId, contentName)
-        }
+      function like (contentType, contentId, contentName) {
+        firebaseService.like(sessionStorage.skyStream_username, contentType, contentId, contentName)
+      }
 
-        var userService = {
-            register: firebaseService.register,
-            login: login,
-            isLoggedIn: isLoggedIn,
-            retrieve: retrieve,
-            update: firebaseService.update,
-            logout: logout,
-            like: like,
-            dislike: dislike
+      function dislike (contentType, contentId, contentName) {
+        firebaseService.dislike(sessionStorage.skyStream_username, contentType, contentId, contentName)
+      }
 
-        }
+      var userService = {
+        register: firebaseService.register,
+        login: login,
+        isLoggedIn: isLoggedIn,
+        retrieve: retrieve,
+        update: firebaseService.update,
+        logout: logout,
+        like: like,
+        dislike: dislike,
+        photo: photo
 
-        return userService
+      }
+
+      return userService
     })
